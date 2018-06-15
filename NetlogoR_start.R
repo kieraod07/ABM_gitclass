3bfc6f776843616ea4f4271d13344e705ab93ab0
Kiera O'Donnell <kieraodonnell@Kieras-MacBook-Pro.local>
2018-06-15 09:35:12 -0400

    initial commit

---
+++ NetlogoR_start.R
@@ -0,0 +1,12 @@
+# introduction to NetlogoR
+
+rm(list = ls())
+
+#install.packages("devtools")
+devtools::install_github("PredictiveEcology/NetLogoR")
+# error - 
+install.packages("quickPlot")
+install.packages("SpaDES.tools")
+# try install again - worked
+
+
